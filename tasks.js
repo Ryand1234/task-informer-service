d62f4770eebe812aade5457b2382c6ac9d971764
Riyan Dhiman <Riyandhiman14@gmail.com>
2020-09-13 13:59:23 +0530

    Only task need to be done within an hour will be send

--- tasks.js
+++ tasks.js
@@ -2,7 +2,8 @@ const Task = require('./model');
 const transporter = require('./transport');
 
 const checkForTask = async () =>{
-	task = await Task.findOne({date: {$lte: Date.now()}}).sort({priority: -1}).limit(1);
+	const time = Date.now() + 3600000;
+	task = await Task.findOne({date: {$lte: time}}).sort({priority: -1}).limit(1);
 	if(task == null){
 		console.log("NO Data");
 		setTimeout(()=>{checkForTask()}, 60000);
